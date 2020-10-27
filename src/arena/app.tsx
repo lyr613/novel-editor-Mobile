7b9bde86493987d914ff9df5487582bc2eabb42c
qsoft <qiusoft613@gmail.com>
2020-10-27 18:12:15 +0800

    feat: 添加安卓返回键监听

--- src/arena/app.tsx
+++ src/arena/app.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { SafeAreaView, StatusBar } from 'react-native'
+import React, { useEffect } from 'react'
+import { BackHandler, SafeAreaView, StatusBar } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
 
-import { NativeRouter, Route, Switch } from 'react-router-native'
+import { NativeRouter, Route, Switch, useHistory } from 'react-router-native'
 import Chapter from './chapter'
 import Shelf from './shelf'
 import TrySome from './trysome'
@@ -13,15 +13,35 @@ const App = () => {
             <StatusBar barStyle="dark-content" />
             <SafeAreaView style={ss.rootbox}>
                 <NativeRouter>
-                    <Switch>
-                        <Route path="/qqq"></Route>
-                        <Route path="/shelf" component={Shelf}></Route>
-                        <Route path="/chapter" component={Chapter}></Route>
-                        <Route component={TrySome}></Route>
-                    </Switch>
+                    <RT />
                 </NativeRouter>
             </SafeAreaView>
         </>
+    )
+}
+
+function RT() {
+    const rt = useHistory()
+    useEffect(() => {
+        // 监听安卓返回键
+        BackHandler.addEventListener('hardwareBackPress', function () {
+            if (rt.location.pathname !== '/') {
+                rt.goBack()
+                return true
+            }
+            /**
+             * 返回false时会使事件继续传递，触发其他注册的监听函数，或是系统默认的后退行为
+             */
+            return false
+        })
+    }, [])
+    return (
+        <Switch>
+            <Route path="/qqq"></Route>
+            <Route path="/shelf" component={Shelf}></Route>
+            <Route path="/chapter" component={Chapter}></Route>
+            <Route component={TrySome}></Route>
+        </Switch>
     )
 }
 
